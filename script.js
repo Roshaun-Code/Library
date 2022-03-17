87c5548eda3a15bb2f7ad8ae2212078427086112
Roshaun <roshaunmarshall09@gmail.com>
2022-03-17 15:46:37 -0500

    Added basic form to collect input

---
+++ script.js
@@ -0,0 +1,18 @@
+//Array to contain all books
+let myLibrary = [];
+
+//Object contructer for a book
+function Book(name){
+    this.name = name;
+}
+
+//Function alls for the object to be pushed in the array
+// function addBookToLibrary(){
+//     let input = prompt("Insert book name");
+//     let book1 = new Book(input);
+//     myLibrary.push(book1);
+// }
+
+// addBookToLibrary();
+// console.log(myLibrary[0].name);
+
