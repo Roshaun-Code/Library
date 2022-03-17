b9a5a70f4c084a854df3e9d5efaf396020627207
Roshaun <roshaunmarshall09@gmail.com>
2022-03-17 16:18:23 -0500

    Finished form basic functionality

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+const openForm = document.querySelectorAll("[data-form-popup]");
+const closeForm = document.querySelectorAll("[data-close-button]");
+const submitButton = document.querySelectorAll("[data-submit-button]")
+const overlay = document.querySelector(".overlay");
+
+openForm.forEach(button => {
+    button.addEventListener('click', () => {
+        const form = document.querySelector(".form-popup")
+        openFormPopup(form)
+    })
+})
+
+
+closeForm.forEach(button => {
+    button.addEventListener('click', () => {
+        const form = document.querySelector(".form-popup")
+        closeFormPopup(form)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const forms = document.querySelectorAll(".form-popup.active")
+    forms.forEach(form => {
+        closeFormPopup(form);
+    })
+})
+
+function openFormPopup(form){
+    if (form == null) return
+    form.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeFormPopup(form){
+    form.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+/* End of form stuff */
+
 //Array to contain all books
 let myLibrary = [];
 
